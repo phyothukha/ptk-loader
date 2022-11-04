c4bb926b24c0c88ce2fb3b3818d52370659af29c
phyothukha <phyothukha2193@gmail.com>
2022-11-04 22:57:51 +0630

    second-update

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ export function removeLoader(){
 export function showProgress(){
     const progressDiv=document.createElement('div');
     progressDiv.classList.add('.top-progress');
-    progressDiv.innerHTML=`<div class="vh-100 d-flex justify-content-center align-item-center fixed-top">
-        <div class="progress">
+    progressDiv.innerHTML=`
+        <div class="progress fixed-top" style="height:10px">
         <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
         </div>
-        </div>
+       
     `
     document.body.append(progressDiv)
 }
