2618d932f6f80e746b6c6037f36158c3a7339348
phyothukha <phyothukha2193@gmail.com>
2022-11-04 22:30:25 +0630

    update-2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function showLoader(){
+export function showLoader(){
     const loaderDiv=document.createElement('div');
     loaderDiv.classList.add('loader');
     loaderDiv.innerHTML=`<div class="vh-100 d-flex justify-content-center align-item-center fixed-top">
@@ -8,12 +8,12 @@ function showLoader(){
     `
     document.body.append(loaderDiv)
 }
-function removeLoader(){
+export function removeLoader(){
     document.querySelector(".loader").remove();
 
 }
 
-function showProgress(){
+export function showProgress(){
     const progressDiv=document.createElement('div');
     progressDiv.classList.add('.top-progress');
     progressDiv.innerHTML=`<div class="vh-100 d-flex justify-content-center align-item-center fixed-top">
@@ -25,7 +25,7 @@ function showProgress(){
     document.body.append(progressDiv)
 }
 
-function removeProgress(){
+export function removeProgress(){
     document.querySelector(".top-progress").remove()
 }
 
